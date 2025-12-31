0667ed7f76003a72eccd7c3f3de64fbfc5a68bb1
taiba-dev380 <taibaseerat380@gmail.com>
2025-12-31 22:46:22 +0500

    Initial commit

--- script.js
+++ script.js
@@ -13,3 +13,26 @@ new Swiper(".testimonial-swiper",{
   autoplay:{delay:3500}
 });
 
+// Order Form 
+
+document.getElementById("orderForm").addEventListener("submit", function(e){
+  e.preventDefault();
+
+  let name = document.querySelector('input[name="name"]').value;
+  let email = document.querySelector('input[name="email"]').value;
+  let message = document.querySelector('textarea[name="message"]').value;
+
+  let whatsappNumber = "92 341-6120222"; 
+
+  let whatsappMessage = 
+    `New Order Request:%0A
+Name: ${name}%0A
+Email: ${email}%0A
+Message: ${message}`;
+
+  let whatsappURL = `https://wa.me/${whatsappNumber}?text=${whatsappMessage}`;
+
+  window.open(whatsappURL, "_blank");
+});
+
+
